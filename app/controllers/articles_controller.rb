abc0c2712396f8a6ce208697e00699476ac61e8e
gloriacruz <ally.cruz@gmail.com>
2017-08-26 18:56:43 -0700

    Build a basic blog with articles and comments.

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,57 @@
+class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
+  # /articles/
+  def index
+    @articles = Article.all
+  end
+
+  # /articles/:id
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  # /articles/new
+  def new
+    @article = Article.new
+  end
+
+  # /articles/:id/edit
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  # /articles/
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  # /articles/:id
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  # /articles/:id
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path #articles_path = index of articles (root)
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+end
